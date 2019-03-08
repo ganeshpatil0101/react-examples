bddd15588b1aa3e7a621cfd2a36dec93915e18e9
Ganesh Patil <ganeshpatil0101@gmail.com>
2019-03-08 11:09:10 +0530

    Initial commit

---
+++ 05-bugTracker-StateManager/stateManager.js
@@ -0,0 +1,50 @@
+var SM = (function(){
+
+	var _currentState = undefined,
+		_reducer = null,
+		_listeners = [],
+		__init_action = '@@INIT_ACTION';
+
+	function getState(){
+		return _currentState;
+	}
+
+	function subscribe(listener){
+		_listeners.push(listener);
+	}
+
+	function triggerChange(){
+		_listeners.forEach(listener => listener());
+	}
+
+	function dispatch(action){
+		let newState = _reducer(_currentState, action);
+		if (newState === _currentState) return;
+		_currentState = newState;
+		triggerChange();
+	}
+
+	function createStore(reducer){
+		_reducer = reducer;
+		_currentState = _reducer(_currentState, __init_action);
+		return {
+			getState,
+			subscribe,
+			dispatch
+		};
+	}
+
+	function bindActionCreators(actionCreators, dispatch){
+		let result = {};
+		for(let key in actionCreators){
+			result[key] = function(){
+				let action = actionCreators[key].apply(undefined, arguments);
+				dispatch(action);
+			}
+		}
+		return result;
+	}
+
+	return { createStore, bindActionCreators };
+
+})();
